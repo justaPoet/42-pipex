a583e1bf349a6383027e11e7a1700a1a0661af13
fefe <fehim.bouanani@gmail.com>
2024-06-25 20:07:15 +0200

    execve() test OK

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: apoet <apoet@student.42.fr>                +#+  +:+       +#+        */
+/*   By: febouana <febouana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 10:24:58 by apoet             #+#    #+#             */
-/*   Updated: 2024/06/25 12:12:14 by apoet            ###   ########.fr       */
+/*   Updated: 2024/06/25 20:04:33 by febouana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,19 @@
 #include <stdio.h>
 #include "get_next_line.h"
 
+#include <sys/wait.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 
-// void main(int argc, char **argv)
+//! TEST PIPE INT
+// int main(int argc, char **argv)
 // {
 //     int fd[2];
 //     if (pipe(fd) == -1)
 //     {
 //         printf("Error pipex\n");
-//         return ;
+//         return (0);
 //     }
 //     int id;
 //     id = fork();
@@ -34,7 +36,7 @@
 //     if (id == -1)
 //     {
 //         printf("Error fork\n");
-//         return ;
+//         return (0);
 //     }
 //     if (id == 0) //? child process
 //     {
@@ -45,7 +47,7 @@
 //         if (write(fd[1], &x, sizeof(int)) == -1)
 //         {
 //             printf("Error write\n");
-//             return ;
+//             return (0);
 //         }    
 //         close(fd[1]);
 //     }
@@ -56,20 +58,73 @@
 //         if (read(fd[0], &y, sizeof(int)) == -1)
 //         {
 //             printf("Error read\n");
-//             return ;
+//             return (0);
 //         } 
 //         close(fd[0]);
 //         printf("got from child process == %d\n", y);
 //     }
+//     return (0);
 // }
 
-void main(int argc, char **argv)
+//! TEST PIPE CHAR*
+// int main(int argc, char **argv)
+// {
+//     int fd[2];
+//     if (pipe(fd) == -1)
+//     {
+//         printf("Error pipex\n");
+//         return(0) ;
+//     }
+//     int id;
+//     id = fork();
+//     printf("ID == %d\n", id);
+//     if (id == -1)
+//     {
+//         printf("Error fork\n");
+//         return(0) ;
+//     }
+//     if (id == 0) //? child process
+//     {
+//         close(fd[0]);
+//         char *line;
+
+//         int fds = open("infile.txt", O_RDWR);
+//         line = get_next_line(fds);
+//         printf("LINE first == %s", line);
+//         if (write(fd[1], line, ft_strlen(line)) == -1)
+//         {
+//             printf("Error write\n");
+//             return(0) ;
+//         }   
+//         printf("LINE second == %s", line);
+//         close(fd[1]);
+//         free(line);
+//     }
+//     else //? parent process
+//     {
+//         wait(0); //? sert a ren ici mais juste pour tester
+//         close(fd[1]);
+//         char *line2 = get_next_line(fd[0]);
+//         if (line2 == NULL)
+//         {
+//             printf("CAMARCHEPAS\n");
+//             return (0);
+//         }
+//         close(fd[0]);
+//         printf("got from child process == %s\n", line2);
+//         free(line2);
+//     }
+//     return(0) ;
+// }
+
+//! TEST AVEC EXECVE()
+int main(int argc, char **argv)
 {
     int fd[2];
     if (pipe(fd) == -1)
     {
         printf("Error pipex\n");
-        return ;
+        return(0) ;
     }
     int id;
     id = fork();
@@ -77,31 +132,51 @@ void main(int argc, char **argv)
     if (id == -1)
     {
         printf("Error fork\n");
-        return ;
+        return(0) ;
     }
     if (id == 0) //? child process
     {
         close(fd[0]);
         char *line;
 
-        int fds = open("infile.txt", O_RDONLY);
-        read(fds, &line, sizeof(char*));
-        printf("===== %s =====\n", &line);
-        if (write(fd[1], &line, sizeof(char*)) == -1)
+        int fds = open("infile.txt", O_RDWR);
+        line = get_next_line(fds);
+        printf("LINE first == %s", line);
+
+
+        if (dup2(fd[1], STDOUT_FILENO) == -1)
         {
-            printf("Error write\n");
-            return ;
-        }   
+            printf("Error dup2\n");
+            return (0);
+        }
+        char *args[] = {"/usr/bin/ls", "-l", NULL};
+        if (execve(args[0], args, NULL) == -1)
+        {
+            printf("Error execve\n");
+            return (0);
+        }
+        printf("LINE sqecond == %s", line);
         close(fd[1]);
+        free(line);
     }
     else //? parent process
     {
+        wait(0); //! sert a rien ici mais juste pour tester
         close(fd[1]);
         char *line2 = get_next_line(fd[0]);
-        
-
+        if (line2 == NULL)
+        {
+            printf("CAMARCHEPAS\n");
+            return (0);
+        }
         close(fd[0]);
-        printf("got from child process == %s\n", line2);
+        printf("got from child process == %s\n", line2); 
         free(line2);
     }
+    return(0) ;
 }
+//* FONCTIONNE ==> la premiere ligne de "ls -l" s'affiche
+//* manque plus qu'une boucle  
+
+//* rajouter fn init_tabtab()
+//* print_tabtab()
